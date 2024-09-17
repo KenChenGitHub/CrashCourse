46afd89b7b4a9f2819ef675124de72effdc4b264
Ken <kennethchen3@gmail.com>
2024-09-17 10:26:33 -0700

    The rest of Chatper 2 questions

--- Chapter2.py
+++ Chapter2.py
@@ -4,4 +4,37 @@
 
 message = "Hello, Python Crash Course"
 print(message)
-asdfasdfsdaf
+
+#2-2 
+var1 = "Hello"
+print(var1)
+var1 = "No Hello"
+print(var1)
+
+#2-3
+var1 = "KC"
+message = f"Hello {var1}, would you like to learn some Python today?"
+print(message)
+
+#2-4
+var1 = "ken"
+print(var1.lower())
+print(var1.upper())
+print(var1.title())
+
+#2-5
+print('Ken\'s park')
+
+#2-6
+famous_person = "Ken"
+print(f"{famous_person}'s park")
+
+#2-7
+var1 = "   KEN   "
+print(f"\tvar1")
+var1 = var1.rstrip().lstrip()
+print(var1)
+
+#2-8
+filename = "python_notes.txt"
+print(filename.removesuffix('.txt'))

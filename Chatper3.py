6f63ceb5e7f5a9f6ce4f76ce8e28d96325491030
Ken <kennethchen3@gmail.com>
2024-09-25 20:49:14 -0700

    adding chapter 2 and 3

---
+++ Chatper3.py
@@ -0,0 +1,39 @@
+#Chatper 3
+
+#3-1
+names = ['ken', 'mich', 'froppy']
+print(names[0])
+print(names[1])
+print(names[2])
+
+#3-2
+print(f"{names[0].title()} is learning python")
+
+#3-3
+print(f"{names[1].title()} goes for a walk")
+
+#3-4
+family = ['dad', 'mom', 'brother', 'sister']
+print(f"{family[0]}, you are invited")
+
+#3-5
+print(f"{family[0]}, cannot make it anymore")
+family[0] = 'pops'
+print(f"{family[0]}, you are invited")
+
+#3-6
+family.insert(1, 'friend')
+family.insert(0, 'friend 2')
+family.append('friend 3')
+print(family)
+
+#3-7
+family.pop()
+family.pop()
+family.pop()
+family.pop()
+family.pop()
+print(family)
+family.remove('friend 2')
+family.remove('pops')
+print(family)
